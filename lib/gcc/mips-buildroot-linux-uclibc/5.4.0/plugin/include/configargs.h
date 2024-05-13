cc524ff74d0482e525fca03ed15f22e41e5f5f07
lehoangnb <lehoang96nb@gmail.com>
2024-05-13 06:14:37 +0000

    Add compiled file

---
+++ lib/gcc/mips-buildroot-linux-uclibc/5.4.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "./configure --prefix=/root/buildroot-2017.05/output/host/usr --sysconfdir=/root/buildroot-2017.05/output/host/etc --enable-static --target=mips-buildroot-linux-uclibc --with-sysroot=/root/buildroot-2017.05/output/host/usr/mips-buildroot-linux-uclibc/sysroot --disable-__cxa_atexit --with-gnu-ld --disable-libssp --disable-multilib --with-gmp=/root/buildroot-2017.05/output/host/usr --with-mpc=/root/buildroot-2017.05/output/host/usr --with-mpfr=/root/buildroot-2017.05/output/host/usr --with-pkgversion='Buildroot 2017.05' --with-bugurl=http://bugs.buildroot.net/ --disable-libquadmath --disable-libsanitizer --enable-tls --disable-libmudflap --enable-threads --without-isl --without-cloog --with-float=soft --disable-decimal-float --with-arch=mips32 --with-abi=32 --enable-languages=c --with-build-time-tools=/root/buildroot-2017.05/output/host/usr/mips-buildroot-linux-uclibc/bin --enable-shared --disable-libgomp";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "abi", "32" }, { "arch", "mips32" }, { "float", "soft" }, { "llsc", "llsc" } };
